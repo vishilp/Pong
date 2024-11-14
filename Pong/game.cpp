7d8ab21b068c7fb04708f00b265a59ca585a1489
vishilp <vishilthe3rd@gmail.com>
2024-11-13 16:54:45 -0800

    added boundaries for the paddle to move

--- Pong/game.cpp
+++ Pong/game.cpp
@@ -1,4 +1,4 @@
-float player_yposition= 100.f;
+float player_yposition= 0.f;
 
 void simulate_game(Input* input, int height, int width)
 {
@@ -6,13 +6,16 @@ void simulate_game(Input* input, int height, int width)
 	background_render();
 	if (input->keyboard[BUTTON_UP].pressed == true)
 	{
-		if (player_yposition+0.9 < height-500) 
+		int topcap = height / 2 - (0.09 * height);
+		if (player_yposition+0.9 < topcap) 
 			player_yposition +=0.9;
 	}
 	if (input->keyboard[BUTTON_DOWN].pressed == true)
 	{
-		player_yposition -= 0.9;
+		int bottomcap = height / 2 - (0.09 * height);
+		if (player_yposition - 0.9 > -bottomcap)
+			player_yposition -= 0.9;
 	}
-	draw_rect(width/2-50, player_yposition, 0.05, 0.2, 0xFFFFFF);//right rect
-	draw_rect(-width/2 + 50, 0, 0.05, 0.2, 0xFFFFFF);//left rect
+	draw_rect(width/2-50, player_yposition, 0.03, 0.17, 0xFFFFFF);//right rect
+	draw_rect(-width/2 + 50, 0, 0.03, 0.17, 0xFFFFFF);//left rect
 }
